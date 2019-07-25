64a6b4b1e366d1c6944673b2f0bfd64e55d02ad0
Alen <alkuca@gmail.com>
2019-07-25 15:48:41 +0200

    prepare deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require("path")
 
 const app = express();
 const users = require("./routes/api/users");
@@ -10,11 +11,22 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Api Running'));
+
 
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 app.use('/api/asset', asset);
+
+
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 
 const PORT = process.env.PORT || 5000;
 
