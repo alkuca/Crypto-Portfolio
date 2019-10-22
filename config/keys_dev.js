e5eaf9aa10c8d59ca8d164b0140782cd1ff83a8d
alkuca <alkuca@gmail.com>
2019-10-22 21:43:17 +0200

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://alkuca23:alkuca23@altposit-xeudd.mongodb.net/test?retryWrites=true&w=majority',
-    jwtSecret: "mysecrettoken",
-    user: "altposit@gmail.com",
-    pass: "allinlink"
-}
